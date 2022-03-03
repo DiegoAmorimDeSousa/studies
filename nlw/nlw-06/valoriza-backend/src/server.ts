37654ce93e4f4b0747d66516001ded115d0e87af
DiegoDevol <diegoASsousa@gmail.com>
2022-03-03 15:33:38 -0300

    Aula 2 NLW-06

--- nlw/nlw-06/valoriza-backend/src/server.ts
+++ nlw/nlw-06/valoriza-backend/src/server.ts
@@ -1,15 +1,15 @@
-import express from 'express';
+import 'reflect-metadata';
+import express from 'express'; 
+import { router } from './routes';   
+
+import './database';
 
 const app = express();
 
-app.get('/test', (request, response) => {
-    return response.send('Olá NLW');
-});
+app.use(express.json());
 
-app.post('/test-post', (request, response) => {
-    return response.send('Olá NLW método post');
-})
+app.use(router);
 
 app.listen(3000, () => {
-    console.log('Server is running NLW')
+    console.log('Server is running')
 });
